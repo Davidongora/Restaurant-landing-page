28abc74dc333c66601de456671524f26d0174799
David <ongoradavid5@gmail.com>
2023-09-01 22:17:22 +0300

    added pages to be rendered

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import Work from './Components/Work';
+import Testimonial from './Components/Testimonial';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
+// import Navbar from './Components/Navbar';
 function App() {
   return (
     <div className="App">
-      Hello
+      <Home />
+      <About />
+      <Work />
+      <Testimonial/>
+      <Contact />
+      <Footer />
     </div>
   );
 }
